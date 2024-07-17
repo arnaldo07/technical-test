e5c6940af49c1826d91e519a7e1c4f33e16b7e4c
“enmanuel” <enmanueldelgado52@gmail.com>
2024-07-17 13:33:28 -0400

    using a custom navigation type to navigate to details screen

--- app/src/main/java/com/enmanuelbergling/technicaltest/ui/contact/detail/ContactScreen.kt
+++ app/src/main/java/com/enmanuelbergling/technicaltest/ui/contact/detail/ContactScreen.kt
@@ -1,5 +1,6 @@
 package com.enmanuelbergling.technicaltest.ui.contact.detail
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.enmanuelbergling.technicaltest.domain.entity.Coordinates
@@ -7,4 +8,5 @@ import com.enmanuelbergling.technicaltest.domain.entity.Coordinates
 @Composable
 fun ContactRoute(onBack: ()->Unit, onLocate: (Coordinates)->Unit) {
 
+    Text(text = "Contact details")
 }
