9833f8cf074378f5c8f43c29aa6460bdce8315ea
“enmanuel” <enmanueldelgado52@gmail.com>
2024-07-17 10:31:54 -0400

    adding app navigation and app state to be aware of navigation state and window size

---
+++ app/src/main/java/com/enmanuelbergling/technicaltest/ui/maps/MapScreen.kt
@@ -0,0 +1,9 @@
+package com.enmanuelbergling.technicaltest.ui.maps
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MapRoute(onBack: ()->Unit) {
+
+}
---
+++ app/src/main/java/com/enmanuelbergling/technicaltest/ui/maps/navigation/MapNavigation.kt
@@ -0,0 +1,36 @@
+package com.enmanuelbergling.technicaltest.ui.maps.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.enmanuelbergling.technicaltest.domain.entity.Coordinates
+import com.enmanuelbergling.technicaltest.ui.maps.MapRoute
+import kotlinx.serialization.Serializable
+
+fun NavHostController.navigateToMap(
+    coordinates: Coordinates,
+    navOptions: NavOptions? = null,
+) {
+    navigate(
+        route = MapDestination(
+            latitude = coordinates.latitude,
+            longitude = coordinates.longitude
+        ),
+        navOptions = navOptions
+    )
+}
+
+@Serializable
+data class MapDestination(
+    val latitude: String,
+    val longitude: String,
+)
+
+fun NavGraphBuilder.mapScreen(
+    onBack: () -> Unit,
+) {
+    composable<MapDestination> {
+        MapRoute(onBack)
+    }
+}
