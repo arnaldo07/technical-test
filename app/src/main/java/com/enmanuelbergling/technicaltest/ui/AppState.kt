440fc6afd32b4f5d4485a1df67229bff4c4139a6
“enmanuel” <enmanueldelgado52@gmail.com>
2024-07-17 13:33:42 -0400

    adding home screen

--- app/src/main/java/com/enmanuelbergling/technicaltest/ui/AppState.kt
+++ app/src/main/java/com/enmanuelbergling/technicaltest/ui/AppState.kt
@@ -4,13 +4,13 @@ import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun rememberAppState(
     windowSizeClass: WindowSizeClass,
-    navController: NavHostController,
+    navController: NavHostController = rememberNavController(),
 ) = remember(windowSizeClass) {
     AppState(windowSizeClass, navController)
 }
