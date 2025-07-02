77e4e09deca43734b224b26f6d76e09d97ac09ba
arnaldo07 <am3675039@gmail.com>
2025-07-01 23:33:17 -0400

    configurando el la inyeccion de dependecias

---
+++ app/src/main/java/com/enmanuelbergling/technicaltest/di/AppModule.kt
@@ -0,0 +1,70 @@
+package com.enmanuelbergling.technicaltest.di
+
+import android.content.Context
+import androidx.room.Room
+import com.enmanuelbergling.technicaltest.BuildConfig
+import com.enmanuelbergling.technicaltest.data.local.AppDatabase
+import com.enmanuelbergling.technicaltest.data.local.FavoriteDao
+import com.enmanuelbergling.technicaltest.data.remote.GeocodingApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "favorites_database.db"
+        ).build()
+    }
+
+    @Provides
+    fun provideFavoriteDao(db: AppDatabase): FavoriteDao {
+        return db.favoriteDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://maps.googleapis.com/")
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGeocodingApiService(retrofit: Retrofit): GeocodingApiService {
+        return retrofit.create(GeocodingApiService::class.java)
+    }
+}
